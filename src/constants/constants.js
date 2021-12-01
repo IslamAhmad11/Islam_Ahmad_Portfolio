c84f37b878d1b8bdc593b77b1827cdc1513781a1
Islam Ahmad <islamahmed1145@gmail.com>
2021-12-01 19:33:39 +0200

    edit published project's links

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,24 +2,13 @@ import { DiReact, DiAngularSimple, DiJavascript, DiDatabase, DiCss3, DiHtml5 } f
 
 export const projects = [
   {
-    title: 'Task Tracker App',
-    description: "Using React, It includes the react ui as well as JSON-server for our mock backend,\
-                  that helps you make sure no tasks slip through the cracks with everything you have to do in a day.\
-                  And it's not just for personal task tracking. You can manage your team members tasks too.",
-    image: 'images/1.webp',
-    tags: ['React', 'JavaScript'],
-    live: 'https://google.com',
-    code: 'https://github.com/IslamAhmad11/React-Task-Tracker.git',
-    id: 0,
-  },
-  {
     title: 'Real Estate Agency Template',
     description:"{This project UNDER DEVELOPMENT...}\nIt gives visitors the first impression of your business.\
                   The real estate website creates a brand identity for your company online. With a site,\
                   you can deliver your message and content to customers.",
     image: 'images/2.webp',
     tags: ['React', 'JavaScript'],
-    live: 'https://google.com',
+    live: 'https://cranky-pike-89a7c2.netlify.app/',
     code: 'https://github.com/IslamAhmad11/Real_Estate_Agency_Template.git',
     id: 1,
   },
@@ -31,9 +20,20 @@ export const projects = [
                   It's a web designer's responsibility to construct.",
     image: 'images/3.webp',
     tags: ['HTML5', 'CSS3'],
-    live: 'https://google.com',
+    live: 'https://elated-wilson-d1273e.netlify.app',
     code: 'https://github.com/IslamAhmad11/Acme.git',
     id: 2,
+  },
+  {
+    title: 'Task Tracker App',
+    description: "{Kindly check Redme file on for usage}\nIt includes the react ui as well as JSON-server for our mock backend,\
+                  that helps you make sure no tasks slip through the cracks with everything you have to do in a day.\
+                  And it's not just for personal task tracking. You can manage your team members tasks too.",
+    image: 'images/1.webp',
+    tags: ['React', 'JavaScript'],
+    live: 'https://vigilant-hopper-b4e3e5.netlify.app',
+    code: 'https://github.com/IslamAhmad11/React-Task-Tracker.git',
+    id: 0,
   },
   {
     title: 'Task Tracker App',
@@ -42,7 +42,7 @@ export const projects = [
                   And it's not just for personal task tracking. You can manage your team members tasks too.",
     image: 'images/4.webp',
     tags: ['Angular', 'JavaScript'],
-    live: 'https://google.com',
+    live: 'https://vigorous-pasteur-78328b.netlify.app',
     code: 'https://github.com/IslamAhmad11/Angular-Task-Tracker.git',
     id: 3,
   },
